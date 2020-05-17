fb629c136e7996200e00c263f94e850cd4454abb
Simon Andre Ruth <simon-andre.ruth@stud.uni-bamberg.de>
2020-05-17 23:41:40 +0200

    button for adding pers

--- app/src/main/java/com/example/db/MainActivity.kt
+++ app/src/main/java/com/example/db/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.db
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         dbHandler = DBHandler(this, null, null, 1)
 
         viewPersons()
+        fab.setOnClickListener {
+        val intent = Intent(this, AddPersonActivity::class.java)
+            startActivity(intent)
+    }
     }
 
     private fun viewPersons(){
